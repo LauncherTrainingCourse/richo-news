8418f925bf20a520f8a555ba722c3472a017da8c
Richo_Han <Richo_Han@asus.com>
2016-12-13 15:52:36 +0800

    Create news related classes.

---
+++ app/src/main/java/com/example/richo_han/richonews/News.java
@@ -0,0 +1,8 @@
+package com.example.richo_han.richonews;
+
+/**
+ * Created by Richo_Han on 2016/12/13.
+ */
+
+public class News {
+}
---
+++ app/src/main/java/com/example/richo_han/richonews/NewsHelper.java
@@ -0,0 +1,14 @@
+package com.example.richo_han.richonews;
+
+import android.util.Log;
+
+/**
+ * Created by Richo_Han on 2016/12/13.
+ */
+
+public class NewsHelper {
+
+    public void loadNews() {
+        Log.d("Tag", "Loading...");
+    }
+}

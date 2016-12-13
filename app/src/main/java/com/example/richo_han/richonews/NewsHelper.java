ef4f98eac68e586373429ae4e7937539491d8fa2
Richo_Han <Richo_Han@asus.com>
2016-12-13 16:02:28 +0800

    Testing api.

--- app/src/main/java/com/example/richo_han/richonews/NewsHelper.java
+++ app/src/main/java/com/example/richo_han/richonews/NewsHelper.java
@@ -2,6 +2,10 @@ package com.example.richo_han.richonews;
 
 import android.util.Log;
 
+import com.loopj.android.http.*;
+
+import cz.msebera.android.httpclient.Header;
+
 /**
  * Created by Richo_Han on 2016/12/13.
  */
@@ -10,5 +14,28 @@ public class NewsHelper {
 
     public void loadNews() {
         Log.d("Tag", "Loading...");
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://www.google.com", new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                // called before request is started
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Log.d("Tag", "Loading succeeded! Status code: " + statusCode);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+            }
+        });
     }
 }

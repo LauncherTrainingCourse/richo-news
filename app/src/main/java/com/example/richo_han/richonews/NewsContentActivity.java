bfcb16f91ec2ec3e0989e7e86bd3c721eb827454
Richo_Han <Richo_Han@asus.com>
2016-12-14 15:25:57 +0800

    Supporting Up button in actionbar.

--- app/src/main/java/com/example/richo_han/richonews/NewsContentActivity.java
+++ app/src/main/java/com/example/richo_han/richonews/NewsContentActivity.java
@@ -1,12 +1,14 @@
 package com.example.richo_han.richonews;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
 
-public class NewsContentActivity extends Activity {
+public class NewsContentActivity extends AppCompatActivity {
     public final static String EXTRA_NEWS = "com.example.richo_han.richonews.EXTRA_NEWS";
     News mNews;
 
@@ -22,5 +24,22 @@ public class NewsContentActivity extends Activity {
         String meta = mNews.getAuthor() + " / " + mNews.getPublishedAt().split("T")[0];
         ((TextView) findViewById(R.id.news_meta)).setText(meta);
         ((TextView) findViewById(R.id.news_description)).setText(mNews.getDescription());
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }

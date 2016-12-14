625de8230677e63691ce51795b8b942c31aaea43
Richo_Han <Richo_Han@asus.com>
2016-12-14 14:30:44 +0800

    Showing news content based on passed instance.

--- app/src/main/java/com/example/richo_han/richonews/NewsContentActivity.java
+++ app/src/main/java/com/example/richo_han/richonews/NewsContentActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class NewsContentActivity extends Activity {
     public final static String EXTRA_NEWS = "com.example.richo_han.richonews.EXTRA_NEWS";
@@ -16,5 +17,10 @@ public class NewsContentActivity extends Activity {
 
         Intent intent = getIntent();
         mNews = intent.getParcelableExtra(EXTRA_NEWS);
+
+        ((TextView) findViewById(R.id.news_title)).setText(mNews.getTitle());
+        String meta = mNews.getAuthor() + " / " + mNews.getPublishedAt().split("T")[0];
+        ((TextView) findViewById(R.id.news_meta)).setText(meta);
+        ((TextView) findViewById(R.id.news_description)).setText(mNews.getDescription());
     }
 }

55bcfb35418ae2e8d2a5ee1d1c30149c6eb7c33e
Richo_Han <Richo_Han@asus.com>
2016-12-15 10:14:22 +0800

    Show number of available activities using toast.

--- app/src/main/java/com/example/richo_han/richonews/NewsContentActivity.java
+++ app/src/main/java/com/example/richo_han/richonews/NewsContentActivity.java
@@ -1,6 +1,8 @@
 package com.example.richo_han.richonews;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
@@ -11,6 +13,9 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class NewsContentActivity extends AppCompatActivity implements RatingDialogFragment.RatingDialogListener{
     public final static String EXTRA_NEWS = "com.example.richo_han.richonews.EXTRA_NEWS";
@@ -60,7 +65,17 @@ public class NewsContentActivity extends AppCompatActivity implements RatingDial
             case R.id.action_browser:
                 Uri webPage = Uri.parse(mNews.getUrl());
                 Intent webIntent = new Intent(Intent.ACTION_VIEW, webPage);
-                startActivity(webIntent);
+
+                // Check the number of apps that are able to deal with the intent.
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(webIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                boolean isIntentSafe = activities.size() > 0;
+                showNumberOfActivities(activities.size());
+
+                if(isIntentSafe) {
+                    startActivity(webIntent);
+                }
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -73,5 +88,14 @@ public class NewsContentActivity extends AppCompatActivity implements RatingDial
         intent.putExtra(EXTRA_NEWS_RATING, rating);
         setResult(RESULT_OK, intent);
         finish();
+    }
+
+    private void showNumberOfActivities(int num) {
+        Context context = getApplicationContext();
+        CharSequence text = "Number of activities: " + num;
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
